7bc30321ffc5863b2abab64a6ec21776bd1d5765
CallMeAnu <anujithbahuleyan@gmail.com>
2020-06-26 16:14:41 +0530

    completed all the assignments

--- Array/Assignment1.cpp
+++ Array/Assignment1.cpp
@@ -3,11 +3,11 @@
 int main()
 {
     int score[5];
-    for (int i = 1;i <= 5;i++){
+    for (int i = 0;i < 5;i++){
         std::cin >> score[i];
     }
     std::cout << "reverse order" << std::endl;
-    for (int num = 1;num <= 5;num++){
-        std::cout << score[num+(num-1)];
+    for (int num = 0;num < 5;num++){
+        std::cout << score[4-num] << std::endl;
     }
 }
--- Array/test.cpp
+++ Array/test.cpp
@@ -6,8 +6,10 @@ int main()
     char code[10] = {'a','b','c','d','e'};
     float wgpa[10] = {1.0,2.1,3.2,4.3,5.4};
     int num[] = {1,2};
-    std::cout << score << std::endl;
-    std::cout << code << std::endl;
-    std::cout << wgpa << std::endl;
-    std::cout << num << std::endl;
+    for (int i = 0;i < 10;i++){
+	std::cout << score[i] << std::endl;
+	std::cout << code[i]<< std::endl;
+	std::cout << wgpa[i]<< std::endl;
+	std::cout << num[i] << std::endl;
+   }
 }
