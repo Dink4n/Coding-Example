6936d66da20f89c93158b325cff7bf8bde687384
CallMeAnu <anujithbahuleyan@gmail.com>
2020-06-27 21:56:04 +0530

    Solved the selection sorting problem :)

--- Sorting/selectionSorting.cpp
+++ Sorting/selectionSorting.cpp
@@ -2,4 +2,20 @@
 
 int main()
 {
-    int list[5], low;
+    int list[5] = {40,32,15,70,1}, low, low_index;
+    for (int i = 1;i < 5;i++){
+        low_index = i-1;
+        for (int num = i;num < 5;num++){
+            if (list[num] < list[low_index]){
+                low_index = num;
+            }
+        }
+        low = list[low_index];
+        list[low_index] = list[i-1];
+        list[i-1] = low;
+    }
+  for (int loop = 0;loop < 5;loop++){
+      std::cout << list[loop] << std::endl;
+   }
+}
+
