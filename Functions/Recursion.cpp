45deef473b56ab1eb1f5c7a41425fb453a5e5cdd
CallMeAnu <anujithbahuleyan@gmail.com>
2020-07-06 17:41:22 +0530

    got the recursion work done

---
+++ Functions/Recursion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int factorial(int n){
+    if ((n == 1) or (n == 0)){
+        return 1;
+    } else {
+        return (n * factorial(n-1));
+    } 
+}
+
+int main(){
+    std::cout << factorial(5) << std::endl;
+    return 0;
+}
