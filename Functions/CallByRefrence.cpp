501eaacf73181a426cadda937984138036cde625
CallMeAnu <anujithbahuleyan@gmail.com>
2020-07-06 17:25:49 +0530

    Created some functions

---
+++ Functions/CallByRefrence.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void change(int & n){
+    n += 1;
+}
+int main(){
+    int x = 0;
+    std::cout << "X in main before : " << x << std::endl;
+    change(x);
+    std::cout << "X in main after Change : " << x << std::endl;
+}
